38c06e6aed90cbfc56576450ce282e0213782318
Sebastian Veuskens <sebastian.veuskens@googlemail.com>
2024-01-30 00:47:05 +0100

    Delete old files

--- my_module/file1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from file2 import MyClass 
-
-a = MyClass()
-print(a.talk())
--- my_module2/file3.py
+++
@@ -1,7 +0,0 @@
-import sys 
-sys.path.append('G:/My Drive/Sonstiges/Projects/battleship')
-from my_module.file2 import MyClass 
-
-
-a = MyClass()
-print(a.talk())
